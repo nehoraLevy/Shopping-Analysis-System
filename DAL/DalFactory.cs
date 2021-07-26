4f4fda6fa0c997a4a8169c6ddde51f9308a6fb8a
batyaLasry <73178144+batyaLasry@users.noreply.github.com>
2021-07-26 12:15:49 +0200

    dalFactoryלא עובד

---
+++ DAL/DalFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class DalFactory
+    {
+        private static IQRCodeFetcher _qRCodeFetcher;
+        private static IDb _db;
+        // TODO: Remove static!!
+        public IQRCodeFetcher GetQRCodeFetcher()
+        {
+            if (_qRCodeFetcher is null)
+            {
+                _qRCodeFetcher = new GoogleDrive.QRCodeFetcher();
+            }
+            return _qRCodeFetcher;
+        }
+        public IDb GetDb()
+        {
+            if (_db is null)
+            {
+                _db = new EntityFramework.HomeEconomicContext();
+            }
+            return _db;
+        }
+    }
+}
