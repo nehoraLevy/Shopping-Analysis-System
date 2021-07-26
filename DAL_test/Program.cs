581beb2a8a6e6706251442208d123acbd5029ce2
נהורה לוי <nehora20@gmail.com>
2021-07-26 14:19:13 +0300

    sdfg

--- DAL_test/Program.cs
+++ DAL_test/Program.cs
@@ -7,8 +7,9 @@ namespace DAL_test
         static void Main(string[] args)
         {
             //Console.WriteLine(DateTime.Now.DayOfYear);
+
             DAL.FireBase.QR_Code.pictureToFireBase();
-            
+            //DAL.FireBase.QR_Code.showDetails("https://firebasestorage.googleapis.com/v0/b/shoppingproject-92d77.appspot.com/o/STAM2.jpg?alt=media&token=3c7bab93-2db0-40a5-8bc2-bc57963c7c19");
             Console.ReadLine();
         }
     }
