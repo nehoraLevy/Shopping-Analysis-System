3ac11436bccbe6cb15285bfb5c40519dc6cba6c3
batyaLasry <73178144+batyaLasry@users.noreply.github.com>
2021-08-05 22:13:59 +0300

    ss

--- BL/DataManagement.cs
+++ BL/DataManagement.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BL
 {
@@ -208,6 +209,12 @@ namespace BL
             }
         }
 
+        public bool ChekEmail(string? email)
+        {
+            string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|" + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)" + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            return regex.IsMatch(email);
+        }
   
         public bool CheckId(int? id)
         {
