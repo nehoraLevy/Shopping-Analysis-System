c889bd384b55dd926961ba1c8b7208779aee5f72
נהורה לוי <nehora20@gmail.com>
2021-08-20 12:44:52 +0300

    start pdf

---
+++ PLWPF/ViewModel/PdfRecommendedVM.cs
@@ -0,0 +1,20 @@
+﻿using PLWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLWPF.ViewModel
+{
+    
+    public class PdfRecommendedVM
+    {
+        AssociationsModel associationModel;
+
+        public PdfRecommendedVM()
+        {
+            associationModel = new AssociationsModel();
+
+
+        }
+    }
+}
